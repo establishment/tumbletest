2ea65c2ba2fdf46fc5928617c6a1e6ebf33b3dec
Alex Velea <veleandu@gmail.com>
2016-06-01 21:32:43 +0300

    Vector utils

---
+++ src/vector_utils.hpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <string>
+#include <ostream>
+
+template<typename T>
+std::vector<T> operator+(const std::vector<T>& lhs, const std::vector<T>& rhs) {
+    std::vector<T> result;
+    result.reserve(lhs.size() + rhs.size());
+    result.insert(result.end(), lhs.begin(), lhs.end());
+    result.insert(result.end(), rhs.begin(), rhs.end());
+    return result;
+}
+
+template<typename T>
+std::vector<T>& operator+=(std::vector<T>& lhs, const std::vector<T>& rhs) {
+    lhs.insert(lhs.end(), rhs.begin(), rhs.end());
+    return lhs;
+}
+
+template<typename T>
+std::vector<T>& operator+=(std::vector<T>& lhs, const T& rhs) {
+    lhs.push_back(rhs);
+
+    return lhs;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& cout, std::vector<T>& rhs) {
+    for (int i = 0; i < int(rhs.size()); i += 1) {
+        if (i != 0) {
+            cout << ' ';
+        }
+        cout << rhs[i];
+    }
+    return cout;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& cout, std::vector<std::vector<T>>& rhs) {
+    for (int i = 0; i < int(rhs.size()); i += 1) {
+        if (i != 0) {
+            cout << '\n';
+        }
+        cout << rhs[i];
+    }
+    return cout;
+}
