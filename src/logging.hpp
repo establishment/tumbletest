de54a45ffcaaf287f38c6ea265a861f7c7a4a3b3
Alex Velea <dev@velea.cc>
2016-10-25 17:56:51 +0300

    Fix a logging bug

--- src/logging.hpp
+++ src/logging.hpp
@@ -23,19 +23,28 @@ void Log(const std::string& file, const std::string& line, const std::string& me
 
 template<>
 void Log<LogType::INFO>(const std::string& file, const std::string& line, const std::string& message) {
-    std::string _file = file.substr(file.find_last_of("/"));
+    std::string _file = file;
+    if (file.find_last_of("/") == std::string::npos) {
+        _file = file.substr(file.find_last_of("/"));
+    }
     std::cerr << Colored(Color::dark_gray, "[INFO]") << _file << ":" << line << "\t> " << message << '\n';
 }
 
 template<>
 void Log<LogType::WARNING>(const std::string& file, const std::string& line, const std::string& message) {
-    std::string _file = file.substr(file.find_last_of("/"));
+    std::string _file = file;
+    if (file.find_last_of("/") == std::string::npos) {
+        _file = file.substr(file.find_last_of("/"));
+    }
     std::cerr << "[WARNING]" << _file << ":" << line << "\t> " << message << '\n';
 }
 
 template<>
 void Log<LogType::ERROR>(const std::string& file, const std::string& line, const std::string& message) {
-    std::string _file = file.substr(file.find_last_of("/"));
+    std::string _file = file;
+    if (file.find_last_of("/") == std::string::npos) {
+        _file = file.substr(file.find_last_of("/"));
+    }
     std::cerr << "[ERROR]" << _file << ":" << line << "\t> " << message << '\n';
     exit(0);
 }
