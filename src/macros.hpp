3a2db6f20ea27a025fa1bc93e15542375da46232
Alex Velea <dev@velea.cc>
2016-12-07 18:26:46 +0200

    Delete some old files

--- src/macros.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "test_archive.hpp"
-
-#include <functional>
-#include <string>
-
-#define addTest(...) tumbletest::test_archive.AddTest(tumbletest::TestCase([=]() -> std::string { return  __VA_ARGS__; }, #__VA_ARGS__))
-#define addCTest(...) tumbletest::test_archive.AddTest(tumbletest::TestCase([=]() -> std::string { __VA_ARGS__; }, #__VA_ARGS__))
